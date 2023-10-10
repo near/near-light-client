0a3aa0dd170a33bdd015f1e74a2b6ad4831b76e8
dndll <donovan@near.org>
2023-10-10 09:39:55 +0100

    chore: fmt

--- src/config.rs
+++ src/config.rs
@@ -16,7 +16,8 @@ pub struct Config {
 impl Config {
     pub fn new() -> Result<Self, ConfigError> {
         let run_mode = env::var("NEAR_LIGHT_CLIENT_NETWORK").unwrap_or_else(|_| "Testnet".into());
-        let default_path = env::var("NEAR_LIGHT_CLIENT_CONFIG_FILE").unwrap_or_else(|_| "default".to_string());
+        let default_path =
+            env::var("NEAR_LIGHT_CLIENT_CONFIG_FILE").unwrap_or_else(|_| "default".to_string());
 
         let s = ConfigTrait::builder()
             .add_source(File::with_name(&default_path))
