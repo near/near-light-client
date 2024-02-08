8d4f8ec85234cb7e5733b4f041255805b44e0e93
dndll <donovan@near.org>
2024-02-08 12:21:26 +0000

    feat!: only read/write trusted header hashes to the circuit BREAKING CHANGE: no use of raw headers in the circuit

--- circuits/plonky2x/src/circuits/sync.rs
+++ circuits/plonky2x/src/circuits/sync.rs
@@ -2,17 +2,13 @@ pub use plonky2x::{self, backend::circuit::Circuit, prelude::*};
 
 use crate::{
     builder::Sync,
-    hint::FetchNextHeaderInputs,
+    hint::{FetchHeaderInputs, FetchNextHeaderInputs},
     variables::{
-        BlockHeightVariable, BuildEndorsement, EncodeInner, HashBpsInputs, HeaderVariable,
+        BlockHeightVariable, BuildEndorsement, CryptoHashVariable, EncodeInner, HashBpsInputs,
     },
 };
 
-// TODO: determine how much we can bootstrap from RB
-// TODO: sync & prove for txs later than sync head
-// TODO: async proof requests, based on a receipt/txs id (should be able to use
-// light client rpc lib TODO: batch proof requests for a set of receipts/txs,
-// must be bounded TODO: batching/experimental proofs
+// TODO: lazy sync
 // TODO[Style]: Shared trait for protocol functionality between crate <> circuit
 // TODO[Style]: macro to share all the same implementation with semantic type
 // differences between protocol crate
@@ -26,29 +22,34 @@ impl<const NETWORK: usize> Circuit for SyncCircuit<NETWORK> {
         <<L as PlonkParameters<D>>::Config as plonky2::plonk::config::GenericConfig<D>>::Hasher:
             plonky2::plonk::config::AlgebraicHasher<<L as PlonkParameters<D>>::Field>,
     {
-        // TODO: decide if we wanna just store the hash here, hint to go get it and then
-        // verify
-        let trusted_head = b.evm_read::<HeaderVariable>();
+        let trusted_header_hash = b.evm_read::<CryptoHashVariable>();
+
+        let untrusted = FetchHeaderInputs(NETWORK.into()).fetch(b, &trusted_header_hash);
+        let untrusted_hash = untrusted.hash(b);
+        b.assert_is_equal(trusted_header_hash, untrusted_hash);
+        let head = untrusted;
 
         // This is a very interesting trick to be able to get the BPS for the next epoch
         // without the need to store the BPS, we verify the hash of the BPS in the
         // circuit
         let bps = FetchNextHeaderInputs(NETWORK.into())
-            .fetch(b, &trusted_head.inner_lite.next_epoch_id)
+            .fetch(b, &head.inner_lite.next_epoch_id)
             .unwrap()
             .next_bps;
 
         let bps_hash = HashBpsInputs.hash(b, &bps);
-        b.assert_is_equal(trusted_head.inner_lite.next_bp_hash, bps_hash);
+        b.assert_is_equal(head.inner_lite.next_bp_hash, bps_hash);
 
-        let head_hash = trusted_head.hash(b);
+        let head_hash = head.hash(b);
         let next_block = FetchNextHeaderInputs(NETWORK.into())
             .fetch(b, &head_hash)
-            .unwrap();
+            .expect("Failed to fetch next block");
         b.watch(&bps_hash, "calculate_bps_hash");
 
-        let synced = b.sync(&trusted_head, &bps, &next_block);
-        b.evm_write::<HeaderVariable>(synced.new_head);
+        let synced = b.sync(&head, &bps, &next_block);
+        b.watch(&synced.new_head, "new_head");
+        let synced_hash = synced.new_head.hash(b);
+        b.evm_write::<CryptoHashVariable>(synced_hash);
     }
 
     fn register_generators<L: PlonkParameters<D>, const D: usize>(registry: &mut HintRegistry<L, D>)
@@ -78,18 +79,10 @@ pub struct ProofMapReduceCtx {
 #[cfg(feature = "beefy-tests")]
 #[cfg(test)]
 mod beefy_tests {
-    use std::str::FromStr;
-
-    use ::test_utils::CryptoHash;
-    use near_light_client_protocol::prelude::Itertools;
-    use near_primitives::types::TransactionOrReceiptId;
     use serial_test::serial;
 
     use super::*;
-    use crate::{
-        test_utils::{builder_suite, testnet_state, B, PI, PO},
-        variables::TransactionOrReceiptIdVariableValue,
-    };
+    use crate::test_utils::{builder_suite, testnet_state, B, PI, PO};
 
     const NETWORK: usize = 1;
 
@@ -97,15 +90,16 @@ mod beefy_tests {
     #[serial]
     fn beefy_test_sync_e2e() {
         let (header, _, _) = testnet_state();
+        let header = header.hash().0;
 
         let define = |b: &mut B| {
             SyncCircuit::<NETWORK>::define(b);
         };
         let writer = |input: &mut PI| {
-            input.evm_write::<HeaderVariable>(header.into());
+            input.evm_write::<CryptoHashVariable>(header.into());
         };
         let assertions = |mut output: PO| {
-            println!("{:#?}", output.evm_read::<HeaderVariable>());
+            let hash = output.evm_read::<CryptoHashVariable>();
         };
         builder_suite(define, writer, assertions);
     }
